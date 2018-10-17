e1e845ef72dcd8abb478eceb7f715e1e151ab0db
sashaWork <oshturniev@griddynamics.com>
2018-10-17 11:42:47 +0300

    update

--- app/src/main/java/edu/uoc/android/restservice/rest/adapter/BaseAdapter.java
+++ app/src/main/java/edu/uoc/android/restservice/rest/adapter/BaseAdapter.java
@@ -1,5 +1,9 @@
 package edu.uoc.android.restservice.rest.adapter;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import edu.uoc.android.restservice.TLSSocketFactory;
 import edu.uoc.android.restservice.rest.contants.ApiConstants;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -17,23 +21,35 @@ class BaseAdapter {
 
     private void init(String baseUrl) {
         if (retrofit == null) {
+            OkHttpClient client = new OkHttpClient();
+            try {
+                client = new OkHttpClient.Builder()
+                        .sslSocketFactory(new TLSSocketFactory())
+                        .build();
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+
             retrofit = new Retrofit.Builder().baseUrl(baseUrl)
-                                             .client(getClient())
-                                             .addConverterFactory(GsonConverterFactory.create())
-                                             .build();
+                    .client(client) // .client(getClient())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
     }
 
-    private OkHttpClient getClient() {
-        OkHttpClient.Builder builderClientHttp = new OkHttpClient().newBuilder();
-        // Show HTTPS logs in dev mode
-        if (ApiConstants.isDebugging) {
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-            interceptor.setLevel(LEVEL_LOG);
-            builderClientHttp.addInterceptor(interceptor);
-        }
-        return builderClientHttp.build();
-    }
+
+//    private OkHttpClient getClient() {
+//        OkHttpClient.Builder builderClientHttp = new OkHttpClient().newBuilder();
+//        // Show HTTPS logs in dev mode
+//        if (ApiConstants.isDebugging) {
+//            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+//            interceptor.setLevel(LEVEL_LOG);
+//            builderClientHttp.addInterceptor(interceptor);
+//        }
+//        return builderClientHttp.build();
+//    }
 
     <T> T createService(Class<T> _class) {
         return retrofit.create(_class);
