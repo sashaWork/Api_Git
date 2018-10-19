bc86b2cd3d80ae059cdbb08838a4a594dca28053
sashaWork <oshturniev@griddynamics.com>
2018-10-19 12:43:03 +0300

    update

---
+++ app/src/main/java/edu/uoc/android/restservice/ExitActivity.java
@@ -0,0 +1,13 @@
+package edu.uoc.android.restservice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ExitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_exit);
+    }
+}
